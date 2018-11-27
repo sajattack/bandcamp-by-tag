2e8e90caf0f24337d700d9eba7d8c0e4612bdccf
Paul Sajna <paulsajna@gmail.com>
2018-11-27 14:48:08 -0800

    add getAlbumsWithTag functionality

--- json-backend/src/index.js
+++ json-backend/src/index.js
@@ -5,6 +5,7 @@ var bandcamp     = require('bandcamp-scraper');
  
 var router = Router(); 
 var handler = Router({mergeParams: true});
+var handler2 = Router({mergeParams: true});
 
 router.get('/', function(req, res) {
   res.setHeader('Content-Type', 'application/json')
@@ -31,6 +32,30 @@ handler.get('/', function (req, res) {
     }
   });
 });
+
+router.use('/albumsWithTag/:tag/:page', handler2);
+
+handler2.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  var params = {
+    "page": parseInt(req.params.page), 
+    "tag": req.params.tag}
+  ; 
+  bandcamp.getAlbumsWithTag(params, function(error, albumsWithTag) {
+    if (error) {
+      res.statusCode = 500;
+      res.end(error);
+    } else {
+      res.statusCode = 200;
+      res.end(JSON.stringify(albumsWithTag));
+    }
+  });
+});
+
+
 
 var server = http.createServer(function(req, res) {
   router(req, res, finalhandler(req, res))
