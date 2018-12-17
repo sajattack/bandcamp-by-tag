8429bd1ce198661bc8d6758efd7eee96811f8e2c
Paul Sajna <sajattack@gmail.com>
2018-12-16 22:03:41 -0800

    convert stateless component to arrow function

--- src/components/TagSelect.jsx
+++ src/components/TagSelect.jsx
@@ -3,15 +3,12 @@ import TagCloud from './TagCloud';
 import FeelingLucky from './FeelingLucky';
 import SearchBar from './SearchBar';
 
-export default class TagSelect extends Component {
-  render() {
-    return (
+const TagSelect = (props) => 
       <div>
         <h1>Select or enter a tag</h1>
         <SearchBar/>
         <TagCloud/>
         <FeelingLucky/>
       </div>
-    );
-  }
-}
+
+export default TagSelect;
